5d564a1893f6bcd96a91499f0487b793927a0761
Robert Blackwood (corp) <blackwoodr@sourcemed.net>
2017-04-26 23:25:43 -0500

    finished initial pass at observable data service

--- src/app/hero.data.service.ts
+++ src/app/hero.data.service.ts
@@ -29,11 +29,31 @@ export class HeroDataService {
   }
 
   save(hero: Hero): void {
-    this.heroHttpService.save(hero).subscribe(savedHero =>{
+    this.heroHttpService.save(hero).subscribe(savedHero => {
+      let heroes = this._heroes.value;
+      
+      let idx = heroes.findIndex(h => h.id === savedHero.id);
+      if(idx > -1) {
+        heroes[idx] = savedHero;
+      }
+      else {
+        heroes.push(savedHero);
+      }
+
+      this._heroes.next(heroes);
     });
   }
 
   delete(hero: Hero): void {
-    this.heroHttpService.delete(hero);
+    this.heroHttpService.delete(hero).subscribe(response =>{
+      let heroes = this._heroes.value;
+
+      let idx = heroes.findIndex(h => h.id === hero.id);
+      if(idx > -1){
+        heroes.splice(idx, 1);
+      }
+
+      this._heroes.next(heroes);
+    });
   }
 }
